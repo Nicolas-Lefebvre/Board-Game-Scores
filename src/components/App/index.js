de1c3a1c2cfd26f1c5c3dbfed1204271d8e2306d
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2023-01-18 12:51:39 +0100

    navbar bootstrap WIP

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,12 @@
+import './styles.scss';
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import Navbar from '../Navbar';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      <Navbar />
     </div>
   );
 }
