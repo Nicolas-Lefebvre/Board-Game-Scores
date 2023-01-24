0b3a501c21e8eff61c3ac9158462e783183d7f3e
Younsi-Amar <younsiamar@hotmail.fr>
2023-01-24 10:43:59 +0100

    liens formulaire

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,12 @@
 import './footer.scss';
+//import { Link } from 'react-router-dom';
 
 // == Composant
 function Footer() {
   return (
     <div className="footer">
 
+      {/*<Link className="contactbutton" to="/contact" role="button">Contact</Link>*/}
       <ul>
         <li><a>Contact</a></li>
         <li><a>CGU</a></li>
