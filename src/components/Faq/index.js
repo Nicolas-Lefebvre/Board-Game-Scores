85834b1b0d07d5b973adbea153ad497fa64b89c2
Younsi-Amar <younsiamar@hotmail.fr>
2023-01-30 10:54:51 +0100

    ajout faq-contact et css mes joueurs

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -3,9 +3,17 @@ import './faq.scss';
 // == Composant
 function Faq() {
   return (
-    <div>
+    <div className="pagefaq">
 
-      <h2>Ici la page FAQ</h2>
+      <h2>Questions fréquentes</h2>
+
+      <ul>
+        <li>Ou puis-je voir mes statistiques?</li>
+        <li>Comment puis-je ajouter un membre de ma famille dans ma liste de joeurs?</li>
+        <li>Comment puis-je ajouter un jeu dans ma liste de jeux?</li>
+        <li>Je n'arrive pas à enregistrer mes statistiques, comment faire?</li>
+        <li>comment vous contacter?</li>
+      </ul>
 
 
     </div>
