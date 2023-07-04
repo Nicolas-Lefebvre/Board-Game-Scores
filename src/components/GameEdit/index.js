c3327906fb4d9a340d6da4287ea22a8007fe39b6
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2023-07-04 12:26:36 +0200

    gameEdit WIP

--- src/components/GameEdit/index.js
+++ src/components/GameEdit/index.js
@@ -1,36 +1,89 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 // import './gameAdd.scss';
 
 import axios from 'axios';
 // import Link from 'antd/es/typography/Link';
+import { Link, useNavigate } from 'react-router-dom';
 
 import {
   Button,
   Form,
+  InputNumber,
+  Radio,
+  Rate,
+  Select,
+  Switch,
   Space,
   Input,
+  // eslint-disable-next-line no-unused-vars
+  Checkbox,
 } from 'antd';
+import { PlusOutlined, MinusCircleOutlined } from '@ant-design/icons';
+import FormItem from 'antd/es/form/FormItem';
 
-import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import Loader from '../Loader';
 
 // Import de la valeur de baseUrl depuis le fichier apiConfig.js
 import baseUrl from '../../apiConfig';
+import { fetchUsersBoardgameList } from '../../actions/boardgames';
+import { fetchPlayerListNoStats } from '../../actions/players';
+import { fetchGameInfos } from '../../actions/games';
+
+const { Option } = Select;
+
+const formItemLayout = {
+  labelCol: { span: 0 },
+  wrapperCol: { span: 20 },
+};
+
+const { TextArea } = Input;
+
+let permission = false;
+const onPermissionChanged = () => {
+  permission = !permission;
+};
 
 // ============================================ Composant===========================================
 function GameEdit() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const queryParameters = new URLSearchParams(window.location.search);
   const gameId = queryParameters.get('game_id');
-  const gameName = queryParameters.get('game_name');
+  console.log('game_id :', gameId);
+
+  // ------------------ Recuperation de la liste des jeux du user -------------------------------------
+  useEffect(() => {
+    dispatch(fetchUsersBoardgameList());
+  }, []);
+  const usersBoardgameListLoaded = useSelector((state) => state.boardgames.usersBoardgameListLoaded);
+  const usersBoardgameList = useSelector((state) => state.boardgames.usersBoardgameList);
+  console.log(usersBoardgameList);
+
+  // ------------------ Recuperation de la liste de tous les joueurs du user --------------------------
+  useEffect(() => {
+    dispatch(fetchPlayerListNoStats());
+  }, []);
+  const usersPlayerList = useSelector((state) => state.players.playerListNoStats);
+  const usersPlayerListLoaded = useSelector((state) => state.players.playerListNoStatsLoaded);
+
+  // ------------------ Recuperation des infos de la partie -------------------------------------------
+  useEffect(() => {
+    // const queryParameters = new URLSearchParams(window.location.search);
+    // const gameId = queryParameters.get('game_id');
+    // console.log(gameId);
+    dispatch(fetchGameInfos(gameId));
+  }, []);
 
   const config = {
     headers: { Authorization: `Bearer ${localStorage.getItem('BGStoken')}` },
   };
-  console.log(gameId);
+
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
-    console.log(gameName);
     axios.patch(
       // URL
       `${baseUrl}/api/user/game/${gameId}`,
@@ -49,38 +102,253 @@ function GameEdit() {
       });
   };
 
+  if (!usersBoardgameListLoaded || !usersPlayerListLoaded) {
+    return <Loader />;
+  }
   return (
     <div className="container addGame-container">
-      <div className="form-container">
-        <section>
-          <h2>Modifier une partie</h2>
-          {/* <h3>Ajouter un nouveau joueur</h3> */}
-          <Form
-            name="validate_new_game"
-            // {...formItemLayout}
-            onFinish={onFinish}
-            // initialValues={{ 'input-number': 3, 'checkbox-group': ['A', 'B'], rate: 3.5 }}
-            // style={{ maxWidth: 2000 }}
-          >
-            <Space>
-              <Form.Item label="Nom du joueur" name="name">
-                <Input
-                  type="text"
-                  defaultValue={gameName}
-                  className="existing-game-input"
-                  name="name"
+      <h2>Ajouter une partie</h2>
+
+      <div>
+        <Form
+          name="validate_other"
+          {...formItemLayout}
+          onFinish={onFinish}
+          initialValues={{ 'input-number': 3, 'checkbox-group': ['A', 'B'], rate: 3.5 }}
+          style={{ maxWidth: 2000 }}
+        >
+
+          {/* ------------------------------------STATUS PARTIE--------------------------------- */}
+          <section style={{ display: 'flex', flexDirection: 'column' }}>
+            <h3>Jeu</h3>
+            <Space style={{ display: 'flex', justifyContent: 'center' }}>
+              <Form.Item name="status" label="Statut partie">
+                <Radio.Group>
+                  <Radio value={true}>Partie terminée</Radio>
+                  <Radio value={false}>Partie en cours</Radio>
+                </Radio.Group>
+              </Form.Item>
+            </Space>
+
+            {/* ------------------------------------SELECTION JEU------------------------------- */}
+            <Space style={{ display: 'flex', justifyContent: 'center' }}>
+              <Form.Item
+                name="boardGame"
+                label="Jeu"
+                hasFeedback
+                rules={[{ required: true, message: 'Selectionnez un jeu' }]}
+              >
+                <Select placeholder="Selectionner un jeu" style={{ minWidth: '200px' }}>
+                  {usersBoardgameList.map((boardgame) => (
+                    <Option
+                      key={boardgame.id}
+                      value={boardgame.id}
+                    >
+                      {boardgame.name}
+                    </Option>
+                  ))}
+                </Select>
+              </Form.Item>
+            </Space>
+
+            <Space style={{ display: 'flex', justifyContent: 'center' }}>
+              <Link to="/jeux/ajouter" style={{ color: 'blue' }}>Ajouter un jeu à ma collection <br /><i style={{ fontStyle: 'italic', color: 'black' }}>(ceci vous amènera sur une nouvelle page)</i></Link>
+            </Space>
+
+            {/* Ce code est désactivé car le jeu en équipe n'est pas encore géré */}
+            {/* Quand ca sera le cas, nous pourrons décommenter cette partie : */}
+            {/* c'est une checkbox qui active le no d'équipe pour chaque joueur lignes 353 à 364 */}
+            {/* <Space style={{ display: 'flex', justifyContent: 'center' }}>
+              <Form.Item
+                label="Jeu en équipes lors de cette partie ?"
+                valuePropName="checked"
+              >
+                <Checkbox
+                  defaultChecked={false}
+                  // disabled={!isTeam}
+                  onChange={() => {
+                    setIsTeam(!isTeam);
+                  }}
                 />
               </Form.Item>
+            </Space> */}
+
+          </section>
+
+          {/* ------------------------------------SELECTION JOUEURS----------------------------- */}
+          <section>
+            <h3>Joueurs</h3>
+            <Form.List
+              name="players"
+              className="players-wrapper"
+              // style={{ display: 'flex', flexWrap: 'wrap', minWidth: "100px" }}
+              initialValue={[
+                {
+                  player: null, score: null, fairplay: 5, isWinner: false, isTeam: isTeam, team: null,
+                },
+                {
+                  player: null, score: null, fairplay: 5, isWinner: false, isTeam: isTeam, team: null,
+                },
+              ]}
+            >
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map(({ key, name, ...restField }) => (
+                    <Space
+                      className="add-player-container"
+                      key={key}
+                      style={{
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        // flexDirection: 'column',
+                        // borderBottom: '1px solid',
+                        marginBottom: 8,
+                        justifyContent: 'center',
+                      }}
+                      align="baseline"
+                    >
+                      <Form.Item
+                        {...restField}
+                        name={[name, 'player']}
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Indiquer le Nom',
+                          },
+                        ]}
+                        label={`Nom joueur ${key + 1}`}
+                      >
+                        {/* <Input placeholder="Nom Joueur" /> */}
+                        <Select placeholder="Selectionner un joueur" style={{ minWidth: '200px' }}>
+                          {usersPlayerList.map((player) => (
+                            <Option
+                              key={player.id}
+                              value={Number(player.id)}
+                            >
+                              {player.name}
+                            </Option>
+                          ))}
+                        </Select>
+                      </Form.Item>
+                      {/* -------------------------Score-------------------------- */}
+                      <Form.Item
+                        {...restField}
+                        name={[name, 'score']}
+                        rules={[
+                          {
+                            required: false,
+                            message: 'Il manque le score',
+                          },
+                        ]}
+                        label={`Score joueur ${key + 1}`}
+                      >
+                        <InputNumber placeholder="Score" />
+                      </Form.Item>
+                      {/* ------------------------isWINNER ------------------------ */}
+                      <Form.Item
+                        label="Vainqueur"
+                        valuePropName="checked"
+                        {...restField}
+                        name={[name, 'isWinner']}
+                      >
+                        <Switch
+                          style={{ backgroundColor: permission ? 'green' : 'orange' }}
+                          checked={permission}
+                          checkedChildren="V"
+                          unCheckedChildren="L"
+                          onChange={(e) => onPermissionChanged(e, permission)}
+                        />
+                      </Form.Item>
+                      {/* ------------------------NOTE ------------------------ */}
+                      <Form.Item name={[name, 'fairplay']} label="Note">
+                        <Rate />
+                      </Form.Item>
+                      {/* ------------------------EQUIPE ----------------------- */}
+                      {isTeam && (
+                      <Form.Item label="N° équipe">
+                        <Form.Item name={[name, 'team']} noStyle>
+                          <InputNumber min={1} max={10} style={{ width: '50px' }} />
+                        </Form.Item>
+                        {/* <span className="ant-form-text" style={{ marginLeft: 8 }}>
+                          n° Equipe
+                        </span> */}
+                      </Form.Item>
+                      )}
+                      {/* ------------------------------------------------------ */}
+                      <MinusCircleOutlined onClick={() => remove(name)} />
+                    </Space>
+                  ))}
+                  <Form.Item style={{
+                    display: 'flex',
+                    // marginBottom: 8,
+                    justifyContent: 'center',
+                  }}
+                  >
+                    <Button type="dashed" style={{ width: '200px', height: '40px', fontWeight: 'bold' }} onClick={() => add()} block icon={<PlusOutlined />}>
+                      Ajouter un joueur
+                    </Button>
+                  </Form.Item>
+                </>
+              )}
+            </Form.List>
+          </section>
+
+          {/* ------------------------------------FIN SELECTION JOUEUR-------------------------- */}
+
+          {/* ------------------------------------SELECTIONS DATES ET HEURE------------------- */}
+          <section>
+            <h3>Date et heure</h3>
+            {/* <Form.Item name="startDate" label="Date et heure de début" {...config}>
+              <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+            </Form.Item>
+            <Form.Item name="endDate" label="Date et heure de fin" {...config}>
+              <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+            </Form.Item> */}
+            <Space>
+              <FormItem name="startDate">
+                <div className="form-group">
+                  <label htmlFor="partieDate">Début partie :
+                    <input type="datetime-local" id="partieDate" name="startDate" />
+                  </label>
+                </div>
+              </FormItem>
             </Space>
             <Space>
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Valider
-                </Button>
-              </Form.Item>
+              <FormItem name="endDate">
+                <div className="form-group">
+                  <label htmlFor="partieDate">Fin partie :
+                    <input type="datetime-local" id="partieDate" name="endDate" />
+                  </label>
+                </div>
+              </FormItem>
             </Space>
-          </Form>
-        </section>
+          </section>
+          {/* ------------------------------------COMMENTAIRE ET PHOTO-------------------------- */}
+          <section>
+            <h3>Commentaires</h3>
+            <Form.Item label="Commentaires" name="comment" className="comment-form-item">
+              <TextArea rows={4} />
+            </Form.Item>
+            {/* <h3>Photo souvenir</h3> */}
+            {/* <Form.Item
+              name="upload"
+              label="Ajouter une photo"
+              valuePropName="fileList"
+              getValueFromEvent={normFile}
+              extra="Formats acceptés"
+            >
+              <Upload name="logo" action="/upload.do" listType="picture">
+                <Button icon={<UploadOutlined />}>Téléverser</Button>
+              </Upload>
+            </Form.Item> */}
+          </section>
+
+          <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+            <Button type="primary" htmlType="submit">
+              Valider
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
 
     </div>
