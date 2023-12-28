f715ea3475c3b5d6cb0c8ed43473048650d88412
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2023-12-29 00:27:15 +0100

    Merge Developpement

---
+++ src/apiConfig.js
@@ -0,0 +1,11 @@
+// apiConfig.js
+
+const baseUrl = 'http://127.0.0.1:8000';
+
+// si en local : http://127.0.0.1:8000
+// Back office : http://127.0.0.1:8000/login
+
+// si en prod : https://boardgamescores.fr/public
+// Bakc office : https://boardgamescores.fr/login
+
+export default baseUrl;
