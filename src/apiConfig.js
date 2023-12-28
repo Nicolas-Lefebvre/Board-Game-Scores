7aa05fd34e64fb1d2116bead4d0dfa71e3e71ada
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2023-12-29 00:47:28 +0100

    Conflit APi resolu

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,10 +1,6 @@
 // apiConfig.js
 
-<<<<<<< HEAD
 const baseUrl = 'http://127.0.0.1:8000';
-=======
-const baseUrl = 'https://boardgamescores.fr/public';
->>>>>>> Developpement
 
 // si en local : http://127.0.0.1:8000
 // Back office : http://127.0.0.1:8000/login
