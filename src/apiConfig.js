3b7c546a4639ca863cc50b9b907e5f645d76b857
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2024-01-09 16:12:56 +0100

    Commit initial apres installaiton

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 // apiConfig.js
 
-const baseUrl = 'http://127.0.0.1:8000';
+const baseUrl = 'https://boardgamescores.fr/public';
 
 // si en local : http://127.0.0.1:8000
 // Back office : http://127.0.0.1:8000/login
