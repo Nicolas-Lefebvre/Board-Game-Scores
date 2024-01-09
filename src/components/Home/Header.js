c5ff94d96c5d1b8b8446ed2dcd89fb4c88ed4e94
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2024-01-09 21:17:35 +0100

    CSS changes on Homepage header

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -3,6 +3,11 @@ function Header() {
   return (
     <div className="header-image">
 
+      <div className="header-image__content">
+        <h1 className="header-image__content__title">Boardgame scores</h1>
+        <p className="header-image__content__text">Le site qui pimente vos parties !</p>
+      </div>
+
       {/* <img src={image} alt="" className="header-image" /> */}
     </div>
   );
