9642c1661dccf5d202ecbf9befe17c7416d1e7fc
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2024-01-10 12:31:51 +0100

    Refonte Connexion OK

--- src/components/Connexion/index.js
+++ src/components/Connexion/index.js
@@ -10,12 +10,8 @@ import {
 // eslint-disable-next-line import/no-extraneous-dependencies
 import jwtDecode from 'jwt-decode';
 import axios from 'axios';
-import {
-  Button,
-  Space,
-  Form,
-  Input,
-} from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
 
 // Import de la valeur de baseUrl depuis le fichier apiConfig.js
 import baseUrl from '../../apiConfig';
@@ -98,76 +94,81 @@ function Connexion() {
     <div className="form-container">
       <section>
         <h2>Connexion</h2>
-          <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600, fontSize: '2rem' }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
           >
-            <Space style={{ minHeight: '100px' }}>
-              <Form.Item
-                label="Adresse Email"
-                placeholder="Adresse Email"
-                name="username"
-                rules={[{ required: true, message: 'Email' }]}
-                style={{ overflow: 'hidden' }}
-              >
-                <Input />
-              </Form.Item>
-            </Space>
-            <Space>
-              <Form.Item
-                label="Mot de passe"
-                placeholder="Mot de passe"
-                name="password"
-                rules={[{ required: true, message: 'Mot de passe' }]}
-              >
-                <Input.Password />
-              </Form.Item>
-            </Space>
-              {/* <Link
-              className="forgetPassword"
-              to="/forgetpassword"
-              role="button">
-                Mot de passe oublié?
-              </Link> */}
-            {/* <Form.Item
-              name="remember"
-              valuePropName="checked"
-              wrapperCol={{ offset: 8, span: 16 }}>
-              <Checkbox>Se rappeler de moi</Checkbox>
-            </Form.Item> */}
-            <Space style={{ justifyContent: 'center' }}>
-              <Form.Item
-                // wrapperCol={{ offset: 8, span: 16 }}
-                style={{ textAlign: 'center' }}
-              >
-                <Button type="primary" htmlType="submit">
-                  Se connecter
-                </Button>
-              </Form.Item>
-            </Space>
-          </Form>
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Se souvenir de moi</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Mot de passe oublié
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Connexion
+            </Button>
+            <span style={{marginRight:'1rem'}}>Pas encore inscrit ?</span>  
+            <a 
+              class Name="mdp-oublie"
+              style={{textDecoration:'underline'}}
+              onClick={() => {
+              navigate('/inscription');
+              }}>
+              Inscrivez-vous!
+            </a>
+          </Form.Item>
+        </Form>
       </section>
 
-    <section>
-      <h2 style={{ marginTop: '2rem' }}>Vous n'êtes pas encore inscrit?</h2>
-      {/* <Link className="btn btn-primary" to="/inscription" role="button"> */}
-        <Button
-          type="primary"
-          htmlType="submit"
-          onClick={() => {
-            navigate('/inscription');
-          }}
-        >
-          Inscrivez vous !
-        </Button>
-      {/* </Link> */}
-    </section>
+      <section>
+        <h2 style={{ marginTop: '2rem' }}>Vous n'êtes pas encore inscrit?</h2>
+        {/* <Link className="btn btn-primary" to="/inscription" role="button"> */}
+          <Button
+            type="primary"
+            htmlType="submit"
+            onClick={() => {
+              navigate('/inscription');
+            }}
+          >
+            Inscrivez vous !
+          </Button>
+        {/* </Link> */}
+      </section>
 
     </div>
   );
